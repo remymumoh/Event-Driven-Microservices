8eada89a5e5599bf407976dc9359ca55af1d0476
Mumo <rmumo@mgic.umaryland.edu>
2022-07-26 10:53:51 +0300

    initial commit

---
+++ src/main/java/com/ornaccodes/eventdrivenmicroservices/EventdrivenmicroservicesApplication.java
@@ -0,0 +1,16 @@
+package com.ornaccodes.eventdrivenmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventdrivenmicroservicesApplication {
+
+    public static void main(String[] args) {
+        
+        SpringApplication.run(EventdrivenmicroservicesApplication.class, args);
+
+        System.out.println("hello world");
+    }
+
+}
---
+++ src/test/java/com/ornaccodes/eventdrivenmicroservices/EventdrivenmicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ornaccodes.eventdrivenmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventdrivenmicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
